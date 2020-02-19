78831367729b120b222ecd5fe4033e5d6d1812d1
GleysondsM <61162711+GleysondsM@users.noreply.github.com>
2020-02-19 16:37:59 -0300

    Add files via upload

---
+++ LigaDaJustica-master/Data/Contexto.cs
@@ -0,0 +1,17 @@
+using LigaDaJustica.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LigaDaJustica.Data
+{
+    public class Contexto : DbContext{
+    
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        {
+
+        }
+                
+        public DbSet<SuperHeroi> SuperHerois {get;set;}
+        public DbSet<SuperVilao> SuperViloes { get; set; }
+
+    }
+}
---
+++ LigaDaJustica-master/Models/SuperVilao.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace LigaDaJustica.Models
+{
+    public class SuperVilao
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Nome { get; set; }
+
+        [Required, Display(Name = "Super Poder")]
+        public string SuperPoder { get; set; }
+    }
+}
